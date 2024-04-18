d4d2f6da5b32e3baf10593ab77cba79f8dcbc9d6
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-04-18 11:26:19 +0900

    Playerクラス追加

---
+++ DirectXGame/Player.h
@@ -0,0 +1,30 @@
+#pragma once
+
+/// <summary>
+/// プレイヤー
+/// </summary>
+class Player {
+
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+private:
+	//ワールド変換データ
+	
+
+};
+

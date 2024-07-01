2deb51726d305a100a8f0dcfbbcef43a92acb877
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-07-01 18:06:19 +0900

    コミット

--- DirectXGame/Skeleton.cpp
+++ DirectXGame/Skeleton.cpp
@@ -40,10 +40,10 @@ void Skeleton::Initialize() {
 	born_[0] = { {10,0,0}, {0,0,0},{0,0,0},0 };
 	born_[0].length = MyMath::Length(born_[0].tip - born_[0].root);
 	born_[0].worldMatrix = MatrixMath::MakeIdentity4x4();
-	born_[1] = { {20,0,0}, {0,0,0},{0,0,0},0};
+	born_[1] = { {10,0,0}, {0,0,0},{0,0,0},0 };
 	born_[1].length = MyMath::Length(born_[1].tip - born_[1].root);
 	born_[1].worldMatrix = MatrixMath::MakeIdentity4x4();
-	
+
 	//ボーンの最大・最小回転角の初期化
 	maxBornRotation_ = sqrtf(powf(born_[0].length, 2) + powf(born_[1].length, 2) - 2 * born_[0].length * born_[1].length * cosf(170));
 	minBornRotation_ = sqrtf(powf(born_[0].length, 2) + powf(born_[1].length, 2) - 2 * born_[0].length * born_[1].length * cosf(30));
@@ -53,62 +53,119 @@ void Skeleton::Update() {
 
 	//==================================== 1bornIK ==================================//
 
-	born_[0].xyLength = sqrtf(powf(born_[0].tip.x, 2) + powf(born_[0].tip.y, 2));
-	born_[0].yzLength = sqrtf(powf(born_[0].tip.y, 2) + powf(born_[0].tip.z, 2));
+	//born_[0].xyLength = sqrtf(powf(born_[0].tip.x, 2) + powf(born_[0].tip.y, 2));
+	//born_[0].yzLength = sqrtf(powf(born_[0].tip.y, 2) + powf(born_[0].tip.z, 2));
 
-	//ピッチ回転(X軸)計算
-	born_[0].rotation.x = atan2(born_[0].tip.z / born_[0].length, born_[0].xyLength / born_[0].length);
-
-	//ヨー回転(Y軸)計算
-	born_[0].rotation.y = atan2(born_[0].tip.y / born_[0].xyLength, born_[0].tip.x / born_[0].xyLength);
-
-	//ロール回転(z軸)計算
-	born_[0].rotation.z = atan2(born_[0].tip.y / born_[0].yzLength, born_[0].tip.z / born_[0].yzLength);
-
-
-	//==================================== 2bornIK ==================================//
-
-	//ボーン全体の長さの初期化
-	float bornLength = born_[0].length + born_[1].length;
-
-	//born0の角度の計算
-	float born0RotationCos;
-	born0RotationCos= cos(powf(bornLength, 2) + powf(born_[0].length, 2) - powf(born_[1].length, 2)) / 2 * bornLength * born_[0].length;
-	float born0RotationSin;
-	born0RotationSin = sin(sqrtf(1 - powf(born0RotationCos, 2)));
-	//born1の角度の計算
-	float born1RotationCos;
-	born1RotationCos = cos(powf(born_[0].length, 2) + powf(born_[1].length, 2) - powf(bornLength, 2)) / 2 * born_[0].length * born_[1].length;
-	float born1RotationSin;
-	born1RotationSin = sin(sqrtf(1 - powf(born1RotationCos, 2)));
-
-	//第2関節の回転角が余弦定理で求めた三角形abcの内角Cではなく
-	//際には外角のπ-Cとなっている点について考慮しなければならない。
-
-	//ピッチ回転(X軸)計算
-	born_[1].rotation.x = atan2(-born0RotationSin, born0RotationCos);
+	////ピッチ回転(X軸)計算
+	//born_[0].rotation.x = atan2(born_[0].tip.z / born_[0].length, born_[0].xyLength / born_[0].length);
 
 	////ヨー回転(Y軸)計算
-	born_[1].rotation.y = atan2(born1RotationSin, -born1RotationCos);
+	//born_[0].rotation.y = atan2(born_[0].tip.y / born_[0].xyLength, born_[0].tip.x / born_[0].xyLength);
 
 	////ロール回転(z軸)計算
 	//born_[0].rotation.z = atan2(born_[0].tip.y / born_[0].yzLength, born_[0].tip.z / born_[0].yzLength);
 
-	born_[1].root = born_[0].tip;
 
-    // 関節の更新
-    joints_[0]->translation_ = { born_[0].root.x, born_[0].root.y, born_[0].root.z };
-    joints_[1]->translation_ = { born_[1].root.x, born_[1].root.y, born_[1].root.z };
-    
-    joints_[0]->UpdateMatrix();
-    joints_[1]->UpdateMatrix();
-   
-	born_[0].worldMatrix = MatrixMath::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, born_[0].rotation, born_[0].root);
+	//ボーン全体の長さの初期化
+	//float bornLength = born_[0].length + born_[1].length;
+
+	////born0の角度の計算
+	//float born0RotationCos;
+	//born0RotationCos= cos(powf(bornLength, 2) + powf(born_[0].length, 2) - powf(born_[1].length, 2)) / 2 * bornLength * born_[0].length;
+	//float born0RotationSin;
+	//born0RotationSin = sin(sqrtf(1 - powf(born0RotationCos, 2)));
+	////born1の角度の計算
+	//float born1RotationCos;
+	//born1RotationCos = cos(powf(born_[0].length, 2) + powf(born_[1].length, 2) - powf(bornLength, 2)) / 2 * born_[0].length * born_[1].length;
+	//float born1RotationSin;
+	//born1RotationSin = sin(sqrtf(1 - powf(born1RotationCos, 2)));
+
+	////第2関節の回転角が余弦定理で求めた三角形abcの内角Cではなく
+	////際には外角のπ-Cとなっている点について考慮しなければならない。
+
+	////ピッチ回転(X軸)計算
+	//born_[1].rotation.x = atan2(-born0RotationSin, born0RotationCos);
+
+	//////ヨー回転(Y軸)計算
+	//born_[1].rotation.y = atan2(born1RotationSin, -born1RotationCos);
+
+	//////ロール回転(z軸)計算
+	////born_[0].rotation.z = atan2(born_[0].tip.y / born_[0].yzLength, born_[0].tip.z / born_[0].yzLength);
+
+	//born_[1].root = born_[0].tip;
+
+ //   // 関節の更新
+ //   joints_[0]->translation_ = { born_[0].root.x, born_[0].root.y, born_[0].root.z };
+ //   joints_[1]->translation_ = { born_[1].root.x, born_[1].root.y, born_[1].root.z };
+ //   
+ //   joints_[0]->UpdateMatrix();
+ //   joints_[1]->UpdateMatrix();
+ //  
+	//born_[0].worldMatrix = MatrixMath::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, born_[0].rotation, born_[0].root);
+	//born_[1].worldMatrix = MatrixMath::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, born_[1].rotation, born_[1].root);
+
+
+
+	//==================================== 2bornIK ==================================//
+
+		//ボーン1の角度の更新
+	float born1Numerator = powf(born_[1].tip.x, 2) - powf(born_[1].tip.y, 2)
+		+ powf(born_[0].length, 2) - powf(born_[1].length, 2);
+	float born1Denominator = 2 * born_[0].length
+		* sqrtf(powf(born_[1].tip.x, 2) + powf(born_[1].tip.y, 2));
+
+	//atanの範囲が-PI/2 ~ PI/2なので場合分けして計算する
+
+	if (born1Denominator != 0) {
+		float npd1 = born1Numerator / born1Denominator;
+		float acosNpd1 = acosf(npd1);
+		if (born_[1].tip.x >= 0) {
+
+			born_[0].rotation.x = acosNpd1 + atanf(born_[1].tip.y / born_[1].tip.x);
+
+		} else { //xの値がマイナスの時
+			born_[0].rotation.x = acosNpd1 + atanf(born_[1].tip.y / born_[1].tip.x) - std::numbers::pi_v<float>;
+		}
+	}
+
+
+	//ボーン1の根本・先端の更新
+	born_[0].tip.x = born_[0].length * cosf(born_[0].rotation.x);
+	born_[0].tip.y = born_[0].length * sinf(born_[0].rotation.x);
+
+	//ボーン2の角度の更新
+	float born2Numerator = powf(born_[0].length, 2) + powf(born_[1].length, 2)
+		- (powf(born_[1].tip.x, 2) + powf(born_[1].tip.y, 2));
+	float born2Denominator = 2 * born_[0].length * born_[1].length;
+
+	if (born2Denominator != 0) {
+		float npd2 = born2Numerator / born2Denominator;
+		float acosNpd2 = acosf(npd2);
+		born_[1].rotation.x = std::numbers::pi_v<float> +acosNpd2;
+	}
+
+	//ボーン2の根本・先端の更新
+	born_[1].root.x = born_[0].length * cosf(born_[0].rotation.x);
+	born_[1].root.y = born_[0].length * sinf(born_[0].rotation.x);
+	born_[1].tip.x = born_[0].tip.x + born_[1].length * cosf(born_[0].rotation.x + born_[1].rotation.x);
+	born_[1].tip.y = born_[0].tip.y + born_[1].length * sinf(born_[0].rotation.x + born_[1].rotation.x);
+
+	//関節の更新
+	joints_[0]->translation_ = { born_[0].root.x,born_[0].root.y,born_[0].root.z };
+	joints_[1]->translation_ = { born_[1].root.x,born_[1].root.y,born_[1].root.z };
+	joints_[2]->translation_ = { born_[1].tip.x,born_[1].tip.y,born_[1].tip.z };
+
+	joints_[0]->UpdateMatrix();
+	joints_[1]->UpdateMatrix();
+	joints_[2]->UpdateMatrix();
+
 
 	//ボーンの操作
 	ImGui::Begin("born");
-	ImGui::SliderFloat3("born1Translate",&born_[0].tip.x, 0.0f, 90.0f, "%.3f");
-	ImGui::SliderFloat3("born2Translate",&born_[1].tip.x, 0.0f, 90.0f, "%.3f");
+	ImGui::SliderFloat3("born1RootTranslate", &born_[0].root.x, 0.0f, 20.0f, "%.3f");
+	ImGui::SliderFloat3("born1TipTranslate", &born_[0].tip.x, 0.0f, 20.0f, "%.3f");
+	ImGui::SliderFloat3("born2rootTranslate", &born_[1].root.x, 0.0f, 20.0f, "%.3f");
+	ImGui::SliderFloat3("born2Translate", &born_[1].tip.x, 0.0f, 20.0f, "%.3f");
 	ImGui::End();
 }
 
@@ -119,8 +176,8 @@ void Skeleton::Draw(const ViewProjection& viewProjection) {
 	}
 
 	//ライン描画
-	PrimitiveDrawer::GetInstance()->DrawLine3d(born_[0].root,born_[0].tip, {1.0f,0.0f,0.0f,1.0f});
-	PrimitiveDrawer::GetInstance()->DrawLine3d(born_[1].root,born_[1].tip, {0.0f,1.0f,0.0f,1.0f});
+	PrimitiveDrawer::GetInstance()->DrawLine3d(born_[0].root, born_[0].tip, { 1.0f,0.0f,0.0f,1.0f });
+	PrimitiveDrawer::GetInstance()->DrawLine3d(born_[1].root, born_[1].tip, { 0.0f,1.0f,0.0f,1.0f });
 
 
 }
